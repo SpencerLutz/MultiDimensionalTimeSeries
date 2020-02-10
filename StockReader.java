094d98471494ce5b5b2904c7f4ff1d6d4a39b6e5
JohnGunasar <20gunasarj@sayreschool.org>
2020-02-10 18:06:41 -0500

    Update StockReader.java

--- StockReader.java
+++ StockReader.java
@@ -4,25 +4,27 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class StockReader {
-		public static String stockName;
-		public static URL apiURL;
-		public static String jsonString = "";
+		private static String stockName;
+		private static URL apiURL;
+		private static String jsonString = "";
+		private static ArrayList<Double> prices = new ArrayList<Double>();
 	 
 		public StockReader(String stockName) {
 			this.stockName = stockName;
 		}
 		
-		public static void setAPILink() throws MalformedURLException {
-			apiURL = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + stockName + "&interval=5min&outputsize=compact&apikey=3NW99LS85309Q6FH");
+		private static void setAPILink() throws MalformedURLException {
+			apiURL = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + StockReader.stockName + "&interval=5min&outputsize=compact&apikey=3NW99LS85309Q6FH");
 		}
 		
-		public static void setJsonString() throws IOException {
+		private static void setJsonString() throws IOException {
 			StockReader.setAPILink();
 			URLConnection conn = apiURL.openConnection();
 			Scanner apiCallScanner = new Scanner(apiURL.openStream());
@@ -31,33 +33,44 @@ public class StockReader {
 			}
 		}
 		
-		public static double getPrice(String date) throws JSONException, IOException {
+		private static double getPrice(String date) throws JSONException, IOException {
 			StockReader.setJsonString();
 			JSONObject prices = new JSONObject(jsonString);
 			return prices.getJSONObject("Time Series (Daily)").getJSONObject(date).getDouble("4. close");
 		}
-			
-		public static void showJsonString() throws IOException {
-			StockReader.setJsonString();
-			System.out.println(jsonString);
-		}
 		
-		public static boolean jsonObjectExists(String date) throws IOException, JSONException {
+		private static boolean jsonObjectExists(String date) throws IOException, JSONException {
 			StockReader.setJsonString();
 			JSONObject currentObject = new JSONObject(jsonString);
 			return !currentObject.getJSONObject("Time Series (Daily)").isNull(date);
 		}
 		
-		public static void getPriceForLastNDays(int n) throws JSONException, IOException {
+		public static void setPricesForLast(int n) throws JSONException, IOException {
 			for(int i = 0;i<n;i++) {
 				String date = LocalDate.now().plusDays(-i-1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 				if(StockReader.jsonObjectExists(date)) {
-					System.out.println(LocalDate.now().plusDays(-i-1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + " : " + StockReader.getPrice(date)); 
+					prices.add(StockReader.getPrice(date));
 				}
-			}
+			}					
+		}
+		
+		public static String getURL() {
+			return StockReader.apiURL.toString();
+		}
+		
+		public static String getJsonString() {
+			return StockReader.jsonString;
+		}
+		
+		public static ArrayList<Double> getPrices() {
+			return StockReader.prices;
 		}
 		public static void main(String[] args) throws JSONException, IOException {
 			StockReader AppleStockReader = new StockReader("AAPL");
-			StockReader.getPriceForLastNDays(365);
+			AppleStockReader.setPricesForLast(150);
+			ArrayList<Double> prices = AppleStockReader.getPrices();
+			for(int i = 0;i<prices.size();i++) {
+				System.out.println(prices.get(i));
+			}
 		}
 	}
