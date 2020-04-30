7ea122a9af764309496a42408637959ab5212d8a
JohnGunasar <20gunasarj@sayreschool.org>
2020-04-29 23:37:09 -0400

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -1,5 +1,7 @@
 
 
+
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -10,11 +12,11 @@ public class Stock {
 	
 	public static int params = 10;
 	
-	private static double[][] getLastPricesFor(String stockName) throws IOException {
+	public static double[][] getLastPricesFor(String stockName) throws IOException, JSONException {
 		return StockReader.getPrices(stockName);
 	}
 	
-	public static double[] getNextPrices(int n, String stockName) throws IOException {
+	public static double[] getNextPrices(int n, String stockName) throws IOException, JSONException {
 		double[][] lastPrices = getLastPricesFor(stockName);
 		double[] result = new double[n];
 		HypothesisFunction StockFunction = new HypothesisFunction(lastPrices,params);
@@ -25,7 +27,8 @@ public class Stock {
 		}
 		return result;
 	}
-	public static double getMaxPrice(int n,String stockName) throws IOException {
+	
+	public static double getMaxPrice(int n,String stockName) throws IOException, JSONException {
 		double[] prices = getNextPrices(n,stockName);
 		double max = prices[0];
 		for(int i = 0;i<prices.length;i++) {
@@ -35,13 +38,8 @@ public class Stock {
 		}
 		return max;
 	}
-	
-	public static void main(String[] args) throws IOException {
-		double[] AAPL_PRICES = Stock.getNextPrices(100,"AAPL");
-		for(int i = 0;i<AAPL_PRICES.length;i++) {
-			System.out.println(AAPL_PRICES[i]);
-		}
-	}
 }
+ 
+
  
  
